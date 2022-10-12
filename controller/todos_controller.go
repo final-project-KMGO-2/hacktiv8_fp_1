83885c7a95027e5bb2d4469b568114df4aef3c3d
Khoirul Anam <khairulanamstud@gmail.com>
2022-10-12 17:14:07 +0700

    add: bcrypt and content-type helpers

--- controller/todos_controller.go
+++ controller/todos_controller.go
@@ -2,10 +2,16 @@ package controller
 
 import (
 	"hacktiv8_fp_1/common"
+	"hacktiv8_fp_1/entity"
+	"hacktiv8_fp_1/helpers"
 	"hacktiv8_fp_1/service"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+)
+
+var (
+	appJson = "application/json"
 )
 
 type TodosController interface {
@@ -49,14 +55,21 @@ func (c *todosController) GetTodos(ctx *gin.Context) {
 // 	result, err := c.todosService.GetTodoById(ctx.Request.Context())
 // }
 
+func (c *todosController) CreateNewTodo(ctx *gin.Context) {
 
-func (c *todosController) CreateNewTodo(ctx *gin.Context){
+	contentType := helpers.GetContentType(ctx)
+	todo := entity.Todos{}
+	if contentType == appJson {
+		ctx.ShouldBindJSON(&todo)
+	} else {
+		ctx.ShouldBind(&todo)
+	}
 
-	result, err := c.todosService.CreateTodo(ctx.Request.Context())
+	result, err := c.todosService.CreateTodo(ctx.Request.Context()) // testing aja ini
 	if err != nil {
 		res := common.BuildErrorResponse("Bad create request", err.Error(), common.EmptyObj{})
 		ctx.JSON(http.StatusBadRequest, res)
 		return
 	}
-	ctx.JSON(http.StatusCreated, common.BuildResponse(true, "Success", result));
+	ctx.JSON(http.StatusCreated, common.BuildResponse(true, "Success", result))
 }
